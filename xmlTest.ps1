c17b9965f5c95b098c1f3a4879d6283ca44653bf
Jesko Radtke <rvdvjesko@users.noreply.github.com>
2023-03-18 16:42:15 +0100

    New f() ReadFileAndGetInfo

--- xmlTest.ps1
+++ xmlTest.ps1
@@ -1,69 +1,3 @@
-function CreateXMLFile {
-    $xml = New-Object System.Xml.XmlTextWriter("test.xml" , $null)
-    # Formatting
-    $xml.Formatting = 'Indented'
-    $xml.Indentation = 1
-    $Xml.IndentChar = "`t"
-    #
-    $xml.WriteStartDocument()
-
-    $xml.WriteStartElement("Testfälle")
-
-    $xml.WriteStartElement("Scenario")
-
-    $xml.WriteStartElement("GIVEN")
-    $xml.WriteStartElement("Given")
-    $xml.WriteAttributeString("value", "This is a given in a scenario")
-    $xml.WriteEndElement()
-    $xml.WriteStartElement("Given")
-    $xml.WriteAttributeString("value", "This is another given in a scenario")
-    $xml.WriteEndElement()
-    $xml.WriteStartElement("Given")
-    $xml.WriteAttributeString("value", "This is a given another in a scenario")
-    $xml.WriteEndElement()
-    $xml.WriteEndElement()
-
-    $xml.WriteStartElement("WHEN")
-    $xml.WriteStartElement("When")
-    $xml.WriteAttributeString("value", "This is a when in a scenario")
-    $xml.WriteEndElement()
-    $xml.WriteEndElement()
-
-    $xml.WriteEndElement()
-
-    $xml.WriteStartElement("Scenario")
-
-    $xml.WriteStartElement("GIVEN")
-    $xml.WriteStartElement("Given")
-    $xml.WriteAttributeString("value", "This is a given in a scenario")
-    $xml.WriteEndElement()
-    $xml.WriteStartElement("Given")
-    $xml.WriteAttributeString("value", "This is another given in a scenario")
-    $xml.WriteEndElement()
-    $xml.WriteStartElement("Given")
-    $xml.WriteAttributeString("value", "This is a given another in a scenario")
-    $xml.WriteEndElement()
-    $xml.WriteEndElement()
-
-    $xml.WriteStartElement("WHEN")
-    $xml.WriteStartElement("When")
-    $xml.WriteAttributeString("value", "This is a when in a scenario")
-    $xml.WriteEndElement()
-    $xml.WriteEndElement()
-
-    $xml.WriteEndElement()
-
-    $xml.WriteEndElement()
-
-    #End of Document
-    $xml.WriteEndDocument()
-    $xml.Flush()
-    $xml.Close()
-}
-
-
-CreateXMLFile
-
 function GetScenariosFromXML {
     $xmlPath = "test.xml"
     $xml = Select-Xml -Path $xmlPath -XPath "//Scenario"
@@ -79,4 +13,71 @@ function GetScenariosFromXML {
 }
 
 
-GetScenariosFromXML
+# GetScenariosFromXML
+
+function ReadFileAndGetInfos($filePath) {
+    $xml = New-Object System.Xml.XmlTextWriter("test.xml" , $null)
+    # Formatting
+    $xml.Formatting = 'Indented'
+    $xml.Indentation = 1
+    $Xml.IndentChar = "`t"
+    #
+    $xml.WriteStartDocument()
+    $xml.WriteStartElement("Testfälle")
+    $lines = Get-Content $filePath
+    $currentType = ""
+    $scenarioIndex = 0
+    foreach ($line in $lines) {
+        if ($line -match $scenarioPattern) {
+            if ($currentType -ne "Scenario" -and $scenarioIndex -ne 0) {
+                $xml.WriteEndElement()
+                $xml.WriteEndElement()
+            }
+            $xml.WriteStartElement("Scenario")
+            $xml.WriteElementString('Beschreibung',$line)
+            $currentType = "Scenario"
+            $scenarioIndex++
+        } if ($line -match $givenPattern) {
+            if ($currentType -ne "Given") {
+                $xml.WriteEndElement()
+                $xml.WriteStartElement("Given")
+                $xml.WriteElementString('Beschreibung', $line)
+            }
+            if ($currentType -eq "Given") {
+                $xml.WriteElementString('Beschreibung', $line)
+            }
+            $currentType = "Given"
+        } elseif ($line -match $whenPattern) {
+            if ($currentType -ne "When") {
+                $xml.WriteEndElement()
+                $xml.WriteStartElement("When")
+                $xml.WriteElementString('Beschreibung', $line)
+            }
+            if ($currentType -eq "When") {
+                $xml.WriteElementString('Beschreibung', $line)
+            }
+            $currentType = "When"
+        } elseif ($line -match $thenPattern) {
+            if ($currentType -ne "Then") {
+                $xml.WriteEndElement()
+                $xml.WriteStartElement("Then")
+                $xml.WriteElementString('Beschreibung', $line)
+            }
+            if ($currentType -eq "Then") {
+                $xml.WriteElementString('Beschreibung', $line)
+            }
+            $currentType = "Then"
+        }
+    }
+    $xml.WriteEndElement()
+    $xml.WriteEndDocument()
+    $xml.Flush()
+    $xml.Close()
+}
+
+ReadFileAndGetInfos "codeunit.txt"
+
+$scenarioPattern = ".*SCENARIO.*"
+$givenPattern = "\[GIVEN.*"
+$whenPattern = "\[WHEN.*"
+$thenPattern = "\[THEN.*"
