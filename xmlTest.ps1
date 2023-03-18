b6bb42b29cf704e7c4d6488bad9d64ac285b51e6
Jesko Radtke <rvdvjesko@users.noreply.github.com>
2023-03-18 15:36:12 +0100

    created

--- xmlTest.ps1
+++ xmlTest.ps1
@@ -1,21 +1,82 @@
-$xml = New-Object System.Xml.XmlTextWriter("test.xml" ,$null)
-# Formatting
-$xml.Formatting = 'Indented'
-$xml.Indentation = 1
-$Xml.IndentChar = "`t"
-#
-$xml.WriteStartDocument()
+function CreateXMLFile {
+    $xml = New-Object System.Xml.XmlTextWriter("test.xml" , $null)
+    # Formatting
+    $xml.Formatting = 'Indented'
+    $xml.Indentation = 1
+    $Xml.IndentChar = "`t"
+    #
+    $xml.WriteStartDocument()
 
-$xml.WriteComment("Testfälle")
-$xml.WriteStartElement("Scenario")
+    $xml.WriteStartElement("Testfälle")
 
-$xml.WriteStartElement("GIVEN")
+    $xml.WriteStartElement("Scenario")
 
-$xml.WriteEndElement()
+    $xml.WriteStartElement("GIVEN")
+    $xml.WriteStartElement("Given")
+    $xml.WriteAttributeString("value", "This is a given in a scenario")
+    $xml.WriteEndElement()
+    $xml.WriteStartElement("Given")
+    $xml.WriteAttributeString("value", "This is another given in a scenario")
+    $xml.WriteEndElement()
+    $xml.WriteStartElement("Given")
+    $xml.WriteAttributeString("value", "This is a given another in a scenario")
+    $xml.WriteEndElement()
+    $xml.WriteEndElement()
 
-$xml.WriteEndElement()
+    $xml.WriteStartElement("WHEN")
+    $xml.WriteStartElement("When")
+    $xml.WriteAttributeString("value", "This is a when in a scenario")
+    $xml.WriteEndElement()
+    $xml.WriteEndElement()
+
+    $xml.WriteEndElement()
+
+    $xml.WriteStartElement("Scenario")
+
+    $xml.WriteStartElement("GIVEN")
+    $xml.WriteStartElement("Given")
+    $xml.WriteAttributeString("value", "This is a given in a scenario")
+    $xml.WriteEndElement()
+    $xml.WriteStartElement("Given")
+    $xml.WriteAttributeString("value", "This is another given in a scenario")
+    $xml.WriteEndElement()
+    $xml.WriteStartElement("Given")
+    $xml.WriteAttributeString("value", "This is a given another in a scenario")
+    $xml.WriteEndElement()
+    $xml.WriteEndElement()
+
+    $xml.WriteStartElement("WHEN")
+    $xml.WriteStartElement("When")
+    $xml.WriteAttributeString("value", "This is a when in a scenario")
+    $xml.WriteEndElement()
+    $xml.WriteEndElement()
+
+    $xml.WriteEndElement()
+
+    $xml.WriteEndElement()
+
+    #End of Document
+    $xml.WriteEndDocument()
+    $xml.Flush()
+    $xml.Close()
+}
 
 
+CreateXMLFile
+
+function GetScenariosFromXML {
+    $xmlPath = "test.xml"
+    $xml = Select-Xml -Path $xmlPath -XPath "//Scenario"
+    foreach ($node in $xml.Node) {
+        Write-Host "Scenario:", $node.Name
+        foreach ($given in $node.GIVEN.ChildNodes) {
+            Write-Host "Given:", $given.InnerText
+        }
+        foreach ($when in $node.WHEN.ChildNodes) {
+            Write-Host "When:", $when.InnerText
+        }
+    }
+}
 
 
-$xml.WriteEndDocument()
+GetScenariosFromXML
